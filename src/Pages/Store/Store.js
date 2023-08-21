615792b50f6918ff065aefa6c04327f9dcf946c2
krishnaprasad <krishnaprasad8369@gmail.com>
2023-08-21 19:37:12 +0530

    offer cards added

--- src/Pages/Store/Store.js
+++ src/Pages/Store/Store.js
@@ -158,79 +158,57 @@ const Store = () => {
         </h5>
       </div>
 
-      <div className="cards">
+      <div className="cards ">
         <div className="row">
-        <div className="card1">
-
-        </div>
+          <div className="card1"></div>
         </div>
         <div className="row">
-        <div className="card2">
-
-        </div>
+          <div className="card2"></div>
         </div>
         <div className="row">
-        <div className="card3">
-
-        </div>
+          <div className="card3"></div>
         </div>
         <div className="row">
-        <div className="card4">
-                  </div>
-                  </div>
-        
+          <div className="card4"></div>
+        </div>
       </div>
       <div className="offers">
-        <h3 className="title">
-          OFFERS
-        </h3>
+        <h3 className="title">OFFERS</h3>
       </div>
-      <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto flex flex-wrap">
-    <div class="lg:w-5/6 mx-auto">
-      <div class="ofr flex flex-wrap w-full bg-green-900 py-32 px-10 relative mb-4">
-        <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src=""/>
-        <div class="text-center relative z-10 w-full">
-          <h2 class="text-2xl text-white font-large title-font mb-2">40% OFF</h2>
-          <p class="leading-relaxed"></p>
-          <a class="mt-3 text-indigo-500 inline-flex items-center">
-            
-          </a>
-        </div>
-      </div>
-      <div class="flex flex-wrap -mx-2">
-        <div class="px-2 w-1/2">
-          <div class="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
-            <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="https://dummyimage.com/542x460"/>
-            <div class="text-center relative z-10 w-full">
-              <h2 class="text-xl text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
-              <p class="leading-relaxed">Skateboard +1 mustache fixie paleo lumbersexual.</p>
-              <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
+      <div className="ofr-card"></div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-7 py-24 mx- flex flex-wrap">
+          <div className="offr-crd lg:w-5/6 mx-auto">
+            <div className="ofr flex flex-wrap w-full bg-green-900 py-32 px-10 relative mb-4">
+              <div className="text-center relative z-10 w-full">
+                <h2 className=" off-text text-2xl text-white  font-normal title-font mb-2">
+                  40% OFF
+                </h2>
+                <p className="leading-relaxed"></p>
+                <a className="mt-3 text-indigo-500 inline-flex items-center"></a>
+              </div>
             </div>
           </div>
         </div>
-        <div class="px-2 w-1/2">
-          <div class="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
-            <img alt="gallery" class="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src="https://dummyimage.com/542x420"/>
-            <div class="text-center relative z-10 w-full">
-              <h2 class="text-xl text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
-              <p class="leading-relaxed">Skateboard +1 mustache fixie paleo lumbersexual.</p>
-              <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
+      </section>
+      <div className="smcards">
+        <div className="smcard">
+          <div className="cpn-card">
+            <img src="" alt="" />COUPON 1</div>
+          <div className="cpn-card">
+            <img src="" alt="" />COUPON 2</div>
+          
         </div>
+        <div className="by-card">BUY 1 GET 1
+          <img src="" alt="" />
+        </div>
+
       </div>
-    </div>
-  </div>
-</section>
+      <div className="collections">
+        <h2 className="clction-title">COLLECTIONS</h2>
+      </div>
+
+      
     </>
   );
 };
