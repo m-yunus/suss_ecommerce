c44ecf9cc43fc823b2be4456a1bcf27c5d453c7b
Yunus <sanuyoonus@gmail.com>
2023-08-21 11:06:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import RouteLayout from "./Routes/RouteLayout";
+
+
+function App() {
+  return (
+   <>
+
+  <RouteLayout/>
+
+  </>
+  );
+}
+
+export default App;
---
+++ src/Routes/RouteLayout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from '../Pages/Home/Home'
+import Store from '../Pages/Store/Store'
+const RouteLayout = () => {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+
+        <Route path='/' element={<Home/>}/>
+        <Route path='store' element={<Store/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default RouteLayout
