e6a51c701d379503ebdfbd711b51422f7a91d557
krishnaprasad <krishnaprasad8369@gmail.com>
2023-08-25 16:09:15 +0530

    offersection added

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -8,13 +8,11 @@ import scndpic from "../Products/productsimg/scndpic.jpg";
 import trdpic from "../Products/productsimg/trdpic.jpg";
 import frthpic from "../Products/productsimg/frthpic.jpg";
 
-
-
 const Products = () => {
   return (
     <>
       <div className="products">
-        <div className="head-title px-10 ">
+        <div className="head-title px-10  ">
           <div className="title">
             <h3 className="bst-title">Best Selling </h3>
             <div className="starimg">
@@ -35,6 +33,40 @@ const Products = () => {
           <img className="cards-img" src={scndpic} alt="" />
           <img className="cards-img" src={trdpic} alt="" />
           <img className="cards-img" src={frthpic} alt="" />
+        </div>
+        <div className="offers-section px-8">
+          <div>
+          <h1 className="ofr-title">OFFERS</h1>
+          </div>
+          <div className="image-container">
+            <img
+              className="ofr-img"
+              src="https://thumbs.dreamstime.com/b/collage-cheerful-jumping-multinational-people-air-color-background-panorama-excited-174200583.jpg?w=1200"
+              alt=""
+            />
+            <h3 className="ofr-text">40% OFF</h3>
+          </div>
+        </div>
+        <div className="buy-section px-8">
+          <div className="cpns mx-auto">
+            <div className="cpn">
+              COUPON 1 
+              <img src="" alt="" />
+             
+            </div>
+            <div className="cpn ">
+              COUPON 2
+             <img src="" alt="" />
+            </div>
+          </div>
+          <div className="buy mx-auto">
+            <img className="buy-img" src="https://img.freepik.com/free-photo/pretty-young-stylish-sexy-woman-pink-luxury-dress-summer-fashion-trend-chic-style-sunglasses-blue-studio-background-shopping-holding-paper-bags-talking-mobile-phone-shopaholic_285396-2957.jpg?size=626&ext=jpg&ga=GA1.1.2019463417.1683196046&semt=sph" alt="" />
+            <h2 className="buy-text">BUY 1 GET 1</h2>
+
+          </div>
+        </div>
+        <div className="footer-cards">
+          
         </div>
       </div>
     </>
