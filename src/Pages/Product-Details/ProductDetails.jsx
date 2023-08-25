628cee6284f4a4da0b3d05752d580a42b59b9e7c
Akshay <akshaypanampatta8891@gmail.com>
2023-08-25 10:03:50 +0530

    made responsive

--- src/Pages/Product-Details/ProductDetails.jsx
+++ src/Pages/Product-Details/ProductDetails.jsx
@@ -8,16 +8,20 @@ import rightarrow from "../../assets/images/Product-Details/right-arrow.png";
 import fav from "../../assets/images/Product-Details/fav.png";
 import delivery from "../../assets/images/Product-Details/delivery.png";
 import plus from "../../assets/images/Product-Details/plus.png";
+import next from "../../assets/images/Product-Details/next.png"
+
+
 const ProductDetails = () => {
   return (
     <>
       <Navbar />
-      <div className="px-16 flex flex-row ">
-        <div className="flex flex-col">
-          <div>
+      <div className="product-details mt-10 px-16 flex flex-row ">
+        <div className="product-left flex flex-col">
+          <div className="flex items-center justify-end relative">
             <img className="product-image" src={product} alt="" />
+            <img className="next-image right-4 absolute top-1/2" src={next} alt="" />
           </div>
-          <div className="flex flex-row gap-2">
+          <div className="rects flex flex-row gap-2">
             <div className="flex flex-col">
               <div className="small-rect"></div>
               <div className="big-rect"></div>
@@ -53,18 +57,19 @@ const ProductDetails = () => {
             <h2>Yello Clow</h2>
           </div>
           <div className="size flex items-center justify-center ">
-            <h1>Size</h1>
+            <div className="flex flex-row items-center ">
+              <h1>Size</h1>
 
-            <div className="flex flex-">
-              <div className="size-box">S</div>
-              <div className="size-box">M</div>
-              <div className="size-box">L</div>
-              <div className="size-box">XL</div>
+              <div className="flex ">
+                <div className="size-box">S</div>
+                <div className="size-box">M</div>
+                <div className="size-box">L</div>
+                <div className="size-box">XL</div>
+              </div>
+              <img className="dwn-arrow" src={downarrow} alt="" />
             </div>
 
-            <img src={downarrow} alt="" />
-
-            <div className="find flex items-center">
+            <div className="find flex  items-center">
               <p>Find Your Fit Guide</p>
               <img className="rgt-arrow" src={rightarrow} alt="" />
             </div>
@@ -84,7 +89,7 @@ const ProductDetails = () => {
               <a href="">View our Delivery & Returns Policy</a>
             </div>
           </div>
-          <div  className="last-prdct-dtls flex flex-col justify-center ">
+          <div className="last-prdct-dtls flex mb-10 flex-col justify-center ">
             <div className="flex">
               <h3>Product Details</h3>
               <img src={plus} alt="" />
