0268d386a6e63906ce711dca83047a41061e72a5
krishnaprasad <krishnaprasad8369@gmail.com>
2023-08-28 13:38:49 +0530

    video section and team section added

--- src/Routes/RouteLayout.js
+++ src/Routes/RouteLayout.js
@@ -4,6 +4,7 @@ import Home from '../Pages/Home/Home'
 import Store from '../Pages/Store/Store'
 import ProductDetails from '../Pages/Product-Details/ProductDetails'
 import Products from '../Pages/Products/Products'
+import AboutUs from '../Pages/AboutUs/AboutUs'
 const RouteLayout = () => {
   return (
     <>
@@ -13,6 +14,7 @@ const RouteLayout = () => {
           <Route path="store" element={<Store />} />
           <Route path="product-details" element={<ProductDetails/>} />
           <Route path="products" element={<Products />} />
+          <Route path="about" element={<AboutUs />} />
 
         </Routes>
       </BrowserRouter>
