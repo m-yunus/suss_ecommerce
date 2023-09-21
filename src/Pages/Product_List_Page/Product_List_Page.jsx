145d24a8d1df1528a5cfb1527e1c09db85eb1812
Akshay <akshaypanampatta8891@gmail.com>
2023-09-21 14:23:09 +0530

    price section completed

--- src/Pages/Product_List_Page/Product_List_Page.jsx
+++ src/Pages/Product_List_Page/Product_List_Page.jsx
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import Navbar from "../../layout/Navbar";
 import "./Product_List_Page.css"
 import filter from "../../assets/images/Product-List-Page/filter.png"
-import {FaAngleRight} from "react-icons/fa"
+import { FaAngleRight, FaAngleDown, FaAngleUp} from "react-icons/fa";
 
 const Product_List_Page = () => {
   const [openDropdown, setOpenDropdown] = useState(null);
+   const [priceDropdown, setPricedropdown] = useState(false);
+    const [colorDropdownopen, setColordropdownopen] = useState(false);
+   const [minValue, setMinValue] = useState(50);
+   const [maxValue, setMaxValue] = useState(1000);
      const toggleDropdown = (dropdownName) => {
        if (openDropdown === dropdownName) {
        
@@ -16,6 +20,19 @@ const Product_List_Page = () => {
        }
      };
 
+      const handleSliderChange = (e) => {
+        const newValue = parseInt(e.target.value);
+        if (newValue >= minValue && newValue <= 1000) {
+          setMaxValue(newValue);
+        }
+      };
+      const pricedropdowntoggle=()=>{
+        setPricedropdown(!priceDropdown);
+      }
+      const colordropdowntoggle=()=>{
+        setColordropdownopen(!colorDropdownopen)
+        
+      }
   return (
     <>
       <Navbar />
@@ -25,7 +42,7 @@ const Product_List_Page = () => {
             Filter <img className=" h-4" src={filter} alt="" />
           </h5>
           <hr />
-          <div className="relative">
+          <div className="relative mt-2">
             <button
               className=" flex justify-between items-center py-[3px] w-full"
               onClick={() => toggleDropdown("printedTshirt")}
@@ -132,7 +149,7 @@ const Product_List_Page = () => {
               </div>
             )}
           </div>
-          <div className="relative">
+          <div className="relative mb-2">
             <button
               className=" flex justify-between items-center py-[3px] w-full"
               onClick={() => toggleDropdown("jeans")}
@@ -147,6 +164,61 @@ const Product_List_Page = () => {
               </div>
             )}
           </div>
+          <hr />
+
+          <button
+            className="plp-btn w-full flex justify-between my-2 items-center"
+            onClick={pricedropdowntoggle}
+          >
+            Price {priceDropdown ? <FaAngleUp /> : <FaAngleDown />}
+          </button>
+          <hr />
+          {priceDropdown && (
+            <div className="my-4">
+              <input
+                className="mt-2 w-full  rounded-lg appearance-none cursor-pointer "
+                type="range"
+                name="range"
+                id="range"
+                value={maxValue}
+                min="50"
+                max="1000"
+                onChange={handleSliderChange}
+              />
+              <div className="flex justify-between mt-4">
+                <div>
+                  <span className=" mr-1">$</span>
+                  <input
+                    type="number"
+                    name="min"
+                    id="min"
+                    value={minValue}
+                    onChange={(e) => setMinValue(parseInt(e.target.value))}
+                  />
+                </div>
+                <div>
+                  <span className=" mr-1">$</span>
+                  <input
+                    type="number"
+                    name="max"
+                    id="max"
+                    value={maxValue}
+                    onChange={(e) => setMaxValue(parseInt(e.target.value))}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+          <hr />
+
+          <button
+            className="plp-btn w-full flex justify-between my-2 items-center"
+            onClick={colordropdowntoggle}
+          >
+            Colors {colorDropdownopen ? <FaAngleUp /> : <FaAngleDown />}
+          </button>
+          {colorDropdownopen && <div></div>}
+          <hr />
         </div>
       </div>
     </>
