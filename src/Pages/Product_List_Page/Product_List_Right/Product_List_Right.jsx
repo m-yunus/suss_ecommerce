09c83224e4b292495bbec290e36af23e43c234d8
Akshay <akshaypanampatta8891@gmail.com>
2023-09-21 15:28:19 +0530

    folder structure changed left section in product list page completed

---
+++ src/Pages/Product_List_Page/Product_List_Right/Product_List_Right.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Product_List_Right.css"
+
+const Product_List_Right = () => {
+  return (
+    <>
+      <div>Product_List_Right</div>
+    </>
+  );
+}
+
+export default Product_List_Right
